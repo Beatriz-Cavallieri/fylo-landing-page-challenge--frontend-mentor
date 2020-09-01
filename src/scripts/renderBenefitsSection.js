81bb718553823ca860b5a7f9f05bb0435633ae34
Beatriz Cavallieri <bfcavallieri@gmail.com>
2020-08-31 22:37:59 -0300

    Add the Benefits section, style for mobile

---
+++ src/scripts/renderBenefitsSection.js
@@ -0,0 +1,41 @@
+const benefits = document.getElementById("benefits");
+
+const benefitsContent = [
+    {
+        image: "../../images/icon-access-anywhere.svg",
+        title: "Acess your files, anywhere",
+        description: "The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere."
+    },
+    {
+        image: "../../images/icon-security.svg",
+        title: "Security you can trust",
+        description: "2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files."
+    },
+    {
+        image: "../../images/icon-collaboration.svg",
+        title: "Real-time collaboration",
+        description: "Securely share files and folders with friends, family and colleagues for live collaboration. No e-mail attachments required."
+    },
+    {
+        image: "../../images/icon-any-file.svg",
+        title: "Store any type of file",
+        description: "Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared."
+    }
+]
+
+for (let i = 0; i < 4; i++) {
+    const benefit = document.createElement("div");
+    benefit.classList.add("benefit");
+    const image = document.createElement("img");
+    image.setAttribute("src", benefitsContent[i].image);
+    const title = document.createElement("h3");
+    title.textContent = benefitsContent[i].title;
+    const description = document.createElement("p");
+    description.textContent = benefitsContent[i].description;
+
+    benefit.appendChild(image);
+    benefit.appendChild(title);
+    benefit.appendChild(description);
+
+    benefits.appendChild(benefit);
+}
