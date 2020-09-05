01174e15090141ae7f9d5fc3da2baa82b32c5728
Beatriz Cavallieri <bfcavallieri@gmail.com>
2020-09-04 22:55:22 -0300

    Finish section 'Benefits'

--- src/scripts/renderBenefitsSection.js
+++ src/scripts/renderBenefitsSection.js
@@ -10,14 +10,14 @@ const benefitsContent = [
         description: "The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere."
     },
     {
-        image: "../../images/icon-security.svg",
-        title: "Security you can trust",
-        description: "2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files."
-    },
-    {
         image: "../../images/icon-collaboration.svg",
         title: "Real-time collaboration",
         description: "Securely share files and folders with friends, family and colleagues for live collaboration. No e-mail attachments required."
+    },
+    {
+        image: "../../images/icon-security.svg",
+        title: "Security you can trust",
+        description: "2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files."
     },
     {
         image: "../../images/icon-any-file.svg",
